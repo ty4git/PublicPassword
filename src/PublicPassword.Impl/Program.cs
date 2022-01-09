6e906b9c3378fa8db75e13fa99f993e1ce0bde0c
- <->
2022-01-09 13:52:15 +0300

    Impl project was added

---
+++ src/PublicPassword.Impl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PublicPassword.Impl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
