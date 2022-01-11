3f6cadb22ad4058f626b1b7a602ed1ef0bfc016e
ty4git <59339738+ty4git@users.noreply.github.com>
2022-01-11 21:51:47 +0300

    appsettings.json and configuration

---
+++ src/PublicPassword.Impl/CommandLineOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace PublicPassword.Impl
+{
+    internal class CommandLineOptions
+    {
+        [Option(shortName: 'e',
+            longName: "encrypting-text",
+            HelpText = "Text for encryption",
+            MetaValue = "string")]
+        public string EncryptingText { get; set; }
+
+        [Option(shortName: 'p',
+            longName: "password",
+            HelpText = "Password that used for encryption or decryption of text",
+            MetaValue = "string")]
+        public string Password { get; set; }
+    }
+}
--- src/PublicPassword.Impl/Program.cs
+++ src/PublicPassword.Impl/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using CommandLine;
+using Microsoft.Extensions.Configuration;
 
 namespace PublicPassword.Impl
 {
@@ -7,6 +12,27 @@ namespace PublicPassword.Impl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            var password = configuration["password"];
+            var encryptingText = configuration["encrypting-text"];
+
+            using var aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.ASCII.GetBytes(password);
+            aesAlg.IV = new[] { }; //todo: should be random
+            using var memoryStream = new MemoryStream();
+            using var cryptoStream = new CryptoStream(
+                memoryStream, 
+                aesAlg.CreateEncryptor(),
+                CryptoStreamMode.Write);
+            using streamWriter
+
+            //CommandLine.Parser.Default.ParseArguments<CommandLineOptions>(args).WithParsed();
         }
     }
 }
